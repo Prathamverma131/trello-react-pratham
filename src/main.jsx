14ad40f81fe5ae1bffac75f16d93aa61c3d0b4bd
Prathamverma131 <prathamverma131@gmail.com>
2024-08-31 11:58:18 +0530

    Error boundary component commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "./components/ErrorPage/ErrorBoundary.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ErrorBoundary fallback="Oops! 404 There was an error.">
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ErrorBoundary>
 );
